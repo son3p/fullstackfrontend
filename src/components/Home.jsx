38ec21eba2b3d46849734cf14795cf940b351495
Hugo Myllari <hugo.myllari@edu.novia.fi>
2024-04-03 22:42:47 +0300

    Complete front end without styling, talks with rest api and the database

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => (
+    <div className="Homepage">
+        <h1>Homepage</h1>
+        <hr />
+        <p>This is our homepage.</p>
+        <a  className="btn btn-success" href="/about">About</a>
+    </div>
+);
+
+export default Homepage;
---
+++ src/services/UserService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import AuthService from '../services/AuthService';
+
+//When moving to to Azure and connecting the sites to each other we have to change the url
+const USER_API_URL = 'http://localhost:3000/';
+//const USER_API_URL = 'https://kwapinotes2024.azurewebsites.net/'
+
+// The decision about should we have BOTH the management of users AND notes in this service i probably something we would need to think about.
+// Here the aggregate is the user, ie a user has notes!
+class UserService {
+  async fetchTodosForUser() {
+    return await axios.get(USER_API_URL + 'api/todos', { headers: AuthService.authHeader() });
+  }
+
+  async addTodoForUser(todo) {
+    return await axios.post(USER_API_URL + 'api/todos', todo, { headers: AuthService.authHeader() });
+  }
+
+  async changeTodoForUser(todoId, todo) {
+    return await axios.put(USER_API_URL + `api/todos/${todoId}`, todo, { headers: AuthService.authHeader() });
+  }
+  
+}
+
+export default new UserService();
