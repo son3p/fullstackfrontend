e16fb6faf2adcee9f40cd44513e09903b55fa318
Hugo Myllari <hugo.myllari@edu.novia.fi>
2024-04-06 14:44:48 +0300

    Swapped the auth api url

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AuthService from '../services/AuthService';
 
 //When moving to to Azure and connecting the sites to each other we have to change the url
-const USER_API_URL = 'http://localhost:3000/';
+const USER_API_URL = 'https://fullstackrestapi.azurewebsites.net/';
 //const USER_API_URL = 'https://kwapinotes2024.azurewebsites.net/'
 
 // The decision about should we have BOTH the management of users AND notes in this service i probably something we would need to think about.
