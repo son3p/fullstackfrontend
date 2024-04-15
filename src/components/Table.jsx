9bcb821f47ffc38b3b63e9c4a8c8cde23366b514
Ella Seger <ella.seger@edu.novia.fi>
2024-04-15 14:30:11 +0300

    Styled Tasks page

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,39 +1,35 @@
 import PropTypes from "prop-types";
 import { Link } from 'react-router-dom'
 import './css/Table.css'
-const Table = ({ caption, fields, rows, resourceName }) => {
 
+
+const Table = ({ caption, fields, rows, resourceName }) => {
   return (
     <>
-      <table className='table table-bordered table-striped bg-light-subtle border border-dark'>
-        <caption> {caption}</caption>
-        <thead className="fw-bold">
-          <tr>
-            {fields.map((field, key) => (
-              <th key={key}>
-                {field.label}
-                <i className="bi bi-sort-alpha-down"></i>
-              </th>
-            ))
-            }
-          </tr>
-        </thead >
-        <tbody>
-          {rows.map((row, rowKey) => (
-            <tr key={rowKey}>
-              {fields.map((field, fieldKey) => (
-                <td key={fieldKey}> {row[field.name]}</td>
-              ))}
-              <td className="text-center">
-                <Link className="btn btn-dark border border-2 border-dark mr-2 button-5" to={`${resourceName}/${row.id}/update`} state={row}>Edit</Link>
-              </td>
-              <td className="text-center">
-                <Link className="btn btn-dark border border-2 border-dark mr-2 button-6" to={`${resourceName}/${row.id}/delete`} state={row}>Delete</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='list-container'>
+        {caption && <h2>{caption}</h2>}
+        <div className="list-group">
+          <div className="row">
+            {rows.map((row, rowKey) => (
+              <div key={rowKey} className="col-md-6 mb-3">
+                <li className="list-group-item border border-dark rounded">
+                  <div>
+                    {fields.map((field, fieldKey) => (
+                      <div key={fieldKey}>
+                        <strong>{field.label}</strong>: {row[field.name]}
+                      </div>
+                    ))}
+                    <div className=" mt-2">
+                      <Link className="btn btn-dark border border-2 border-dark me-4 button-5" to={`${resourceName}/${row.id}/update`} state={row}>Edit</Link>
+                      <Link className="btn btn-dark border border-2 border-dark  button-6" to={`${resourceName}/${row.id}/delete`} state={row}>Delete</Link>
+                    </div>
+                  </div>
+                </li>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
@@ -43,6 +39,6 @@ Table.propTypes = {
   fields: PropTypes.array.isRequired,
   rows: PropTypes.array.isRequired,
   resourceName: PropTypes.string,
-
-}
+};
 export default Table;
+
--- src/components/todo/Todos.jsx
+++ src/components/todo/Todos.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import UserService from '../../services/UserService.js'
 
 import Table from "../Table.jsx"
+import Footer from "../Footer";
 
 const Todos = () => {
     const [todos, setTodos] = useState([]);
@@ -33,19 +34,23 @@ const Todos = () => {
 
     return (
         <>
-            <div className="m-4 "style={{ fontFamily: 'Courier New' }}>
+            <div className="m-4 " style={{ fontFamily: 'Courier New' }}>
                 <h1 >Tasks</h1>
                 <hr />
             </div>
             <div className="container mt-5 mb-5">
-                <h1 className="mt-5 mb-5 fw-bolder"> User&apos;s Todos </h1>
+                <h1 className="mt-3 mb-3 fw-bolder"> User&apos;s Todos </h1>
 
                 <a className="btn btn-dark border border-2 border-dark button-3 bi-journal-plus" href="/todos/create">Create new todo</a>
-                <div className="table-responsive my-5">
-                    <Table caption={'Todos'} fields={fields} rows={todos} resourceName='/todos'></Table>
-                </div>
-                <button type="button" className="btn btn-dark border border-2 border-dark button-4" onClick={getAllTodos}>Refresh</button>
+                <button type="button" className="btn btn-dark border border-2 border-dark button-4 ms-5" onClick={getAllTodos}>Refresh site</button>
                 {error && <p className="text-danger">{error}</p>}
+                <div className="my-4">
+                    <Table caption={''} fields={fields} rows={todos} resourceName='/todos'></Table>
+                </div>
+
+            </div>
+            <div className="mt-5">
+                <Footer name="Website Made By: Hugo, Kim & Ella. Year 2024. Class: INF23F-Å." styling="text-light text-center border border-dark  p-3"></Footer>
             </div>
         </>
 
