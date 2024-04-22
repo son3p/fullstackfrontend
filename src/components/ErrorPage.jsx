827ab13dd3eb021cf79d3ef1e2c994b0a3462a45
Kim Nymark <kim.nymark@edu.novia.fi>
2024-04-22 15:52:02 +0300

    Task now Todo in header. Also fixed a path.

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => (
     <h1>Oops! Page Not Found</h1>
     <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
     <p>Head back to <Link to="/">Home</Link>.</p>
-    <img src="src\assets\sad-cat-cat-in-front-of-sea.gif" alt="Error 404" className="img-fluid" />
+    <img src="src/assets/sad-cat-cat-in-front-of-sea.gif" alt="Error 404" className="img-fluid" />
   </div>
 );
 
