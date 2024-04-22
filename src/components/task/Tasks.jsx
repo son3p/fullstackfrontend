776bbb39b2886e0ae8604c43641f51ec88c649f7
Ella Seger <ella.seger@edu.novia.fi>
2024-04-22 16:25:21 +0300

    Fixed styling, made currentuser also to Todos page

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,47 +1,54 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, useLocation } from 'react-router-dom'; 
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import UserService from '../../services/UserService.js';
 import Footer from "../Footer.jsx";
 import { Link } from 'react-router-dom'
 //s
 const Tasks = () => {
-    const {todoId} = useParams()
-    const [tasks, setTasks] = useState([]);
-    const [error, setError] = useState('');
+  const { todoId } = useParams()
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState('');
 
 
-    const getAllTasks = () => {
-        UserService.fetchTasksForUser(todoId)
-            .then(res => {
-                setTasks(res.data.data);
-            })
-            .catch(err => {
-                setError(err.message);
-            });
-    }
+  const getAllTasks = () => {
+    UserService.fetchTasksForUser(todoId)
+      .then(res => {
+        setTasks(res.data.data);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
+  }
 
-    useEffect(() => {
-        getAllTasks();
-    }, []);
+  useEffect(() => {
+    getAllTasks();
+  }, []);
 
-    const fields = [
-        { name: 'id', label: 'Id' },
-        { name: 'task', label: 'Task' },
-        { name: 'priority', label: 'Priority' },
-        { name: 'estimated_time', label: 'Estimated time' },
-        { name: 'createdAt', label: 'Creation time ' }
-    ];
+  const fields = [
+    { name: 'id', label: 'Id' },
+    { name: 'task', label: 'Task' },
+    { name: 'priority', label: 'Priority' },
+    { name: 'estimated_time', label: 'Estimated time' },
+    { name: 'createdAt', label: 'Creation time ' }
+  ];
 
-    const rows = tasks
+  const rows = tasks
 
-    return (
-        <>
-        <Link className="btn btn-dark border border-2 border-dark button-3 bi-journal-plus" to={`/todos/${todoId}/task/create`}>Create new todo</Link>
-        <div className='list-container'>
+  return (
+    <>
+      <div className="m-4 " style={{ fontFamily: 'Courier New' }}>
+        <h1>Task Details</h1>
+        <hr />
+        <h3>Here is your task/tasks stored and also can create new ones!</h3>
+      </div>
+      <Link to="/todos" className="btn btn-dark border border-2 border-dark button-14 ms-4 bi bi-arrow-left-short ">Back</Link>
+      
+      <div className='container mt-5 mb-5 '>
+      <Link className="btn btn-dark border border-2 border-dark button-3 bi-journal-plus mb-5 " to={`/todos/${todoId}/task/create`}>Create new task</Link>
         <div className="list-group">
           <div className="row">
             {rows.map((row, rowKey) => (
-              <div key={rowKey} className="col-md-6 mb-3">
+              <div key={rowKey} className="col-md-6 mb-3 ">
                 <li className="list-group-item border border-dark rounded">
                   <div>
                     {fields.map((field, fieldKey) => (
@@ -49,9 +56,9 @@ const Tasks = () => {
                         <strong>{field.label}</strong>: {row[field.name]}
                       </div>
                     ))}
-                    <div className=" mt-2">
-                      <Link className="btn btn-dark border border-2 border-dark  button-6" to={`${row.id}/update`} state={row}>Update</Link>
-                      <Link className="btn btn-dark border border-2 border-dark  button-6" to={`${row.id}/delete`} state={row}>delete</Link>
+                    <div className=" mt-2 ">
+                      <Link className="btn btn-dark border border-2 border-dark me-4  button-15" to={`${row.id}/update`} state={row}>Edit</Link>
+                      <Link className="btn btn-dark border border-2 border-dark me-4 button-16" to={`${row.id}/delete`} state={row}>Delete</Link>
                     </div>
                   </div>
                 </li>
@@ -61,8 +68,11 @@ const Tasks = () => {
           </div>
         </div>
       </div>
-        </>
-    );
+      <div className="mt-5">
+                <Footer name="Website Made By: Hugo, Kim & Ella. Year 2024. Class: INF23F-Å." styling="text-light text-center border border-dark  p-3"></Footer>
+            </div>
+    </>
+  );
 };
 
 export default Tasks;
--- src/components/todo/Todos.jsx
+++ src/components/todo/Todos.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import UserService from '../../services/UserService.js'
-
+import AuthService from '../../services/AuthService.js';
 import Task from '../task/Tasks.jsx'
 
 import Table from "../Table.jsx"
@@ -9,7 +9,7 @@ import Footer from "../Footer";
 const Todos = () => {
     const [todos, setTodos] = useState([]);
     const [error, setError] = useState('');
-
+    const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
 
     // We use this function for both useEffect and handleClick
     // res.data is axios package. the packages data is what we decide to call it
@@ -24,7 +24,8 @@ const Todos = () => {
     }
     // you tell React that your component needs to do something after render with useEffect
     useEffect(() => {
-        getAllTodos()
+        getAllTodos();
+        setCurrentUser(AuthService.getCurrentUser()); // Update current user
     }, []);
 
     const fields = [
@@ -39,10 +40,10 @@ const Todos = () => {
                 <hr />
             </div>
             <div className="container mt-5 mb-5">
-                <h1 className="mt-3 mb-3 fw-bolder"> User&apos;s Todos </h1>
+                <h1 className="mt-3 mb-3 fw-bolder"> {currentUser.username}&apos;s Todos </h1>
 
                 <a className="btn btn-dark border border-2 border-dark button-3 bi-journal-plus" href="/todos/create">Create new todo</a>
-                <button type="button" className="btn btn-dark border border-2 border-dark button-4 mt-2 d-block" onClick={getAllTodos}>Refresh site</button>
+                <button type="button" className="btn btn-dark border border-2 border-dark button-4 mt-2 d-block bi bi-arrow-clockwise" onClick={getAllTodos}>Refresh site</button>
                 {error && <p className="text-danger">{error}</p>}
                 <div className="my-4">
                     <Table caption={''} fields={fields} rows={todos} resourceName='/todos'></Table>
